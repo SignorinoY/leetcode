a215b5e0c6301c263457751d90d8de4d6f3b4b00
SignorinoY <meetziyang@gmail.com>
2020-09-05 14:18:04 +0800

    :sparkles: remove-duplicates-from-sorted-array time cost 44ms (11.13%), memory cost 14.6 mb (6.63%)

---
+++ 26.删除排序数组中的重复项.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode.cn id=26 lang=python3
+#
+# [26] 删除排序数组中的重复项
+#
+
+# @lc code=start
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        for i in range(len(nums) - 1, 0, -1):
+            if nums[i] == nums[i - 1]:
+                nums.pop(i)
+        return len(nums)
+# @lc code=end
+
