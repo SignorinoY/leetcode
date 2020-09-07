60b9fa65455e662b9da1b1be171ec022d5b3cc3b
SignorinoY <meetziyang@gmail.com>
2020-09-07 12:41:33 +0800

    :zap: refactor 23 merge-k-sorted-list time cost 128 ms (56.47%), memory cost 17.8 mb (69.37%)

--- 23.合并k个升序链表.py
+++ 23.合并k个升序链表.py
@@ -10,17 +10,21 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+from heapq import heappop, heappush
+
+
 class Solution:
     def mergeKLists(self, lists: List[ListNode]) -> ListNode:
+        heap = []
+        for node in lists:
+            while node:
+                heappush(heap, node.val)
+                node = node.next
         head = ListNode()
         prev = head
-        while any(item for item in lists):
-            vals = [item.val if item else 1e5 for item in lists]
-            val_min = min(vals)
-            idx = vals.index(val_min)
-            prev.next = lists[idx]
+        while heap:
+            prev.next = ListNode(heappop(heap))
             prev = prev.next
-            lists[idx] = lists[idx].next
         return head.next
 
 # @lc code=end
